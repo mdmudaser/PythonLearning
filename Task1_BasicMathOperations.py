4d451819112439fa6ba6d4c31dfa6f86de0694ca
mdmudaser <mdmudaser@email.com>
2025-09-12 15:19:54 +0530

    PythonLearning Task1&2

---
+++ Task1_BasicMathOperations.py
@@ -0,0 +1,9 @@
+#Perform Basic Math Operations with the input of two numbers
+a = input("Enter the First Number :")
+b = input("Enter the Second Number :")
+a = float(a)
+b = float(b)
+print("Addition :", a+b)
+print("Subtraction :", a-b)
+print("Multiplication : ", a*b)
+print("Division : ", a/b)
---
+++ Task2_GreetingwithFullName.py
@@ -0,0 +1,5 @@
+#Program to input first name and second name and show greetings with full name in output
+first_name = str(input("Enter your First Name: "))
+last_name = str(input("Enter your Second Name: "))
+full_name = first_name + " " + last_name
+print("Hello, " + full_name + "! Welcome to the Python Program")
